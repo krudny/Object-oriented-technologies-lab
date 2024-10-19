329c346f24efd0c994b85d2449a5441937957012
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-10-19 20:23:26 +0200

    Implemented multithreading photodownloading

--- Lab3-reactive-java/src/main/java/app/CrawlerApp.java
+++ Lab3-reactive-java/src/main/java/app/CrawlerApp.java
@@ -7,14 +7,14 @@ public class CrawlerApp {
 
     public static final String SCRAPER_API_KEY = "7811b91612435e2e05f8839ad2d3e13d";
 
-    private static final List<String> TOPICS = List.of("Agent Cooper", "Sherlock", "Poirot", "Detective Monk");
+    private static final List<String> TOPICS = List.of("Airplane", "BigBen", "Agent Cooper", "Sherlock", "Poirot", "Detective Monk");
 
 
     public static void main(String[] args) throws IOException {
         PhotoCrawler photoCrawler = new PhotoCrawler();
         photoCrawler.resetLibrary();
-        photoCrawler.downloadPhotoExamples();
+//        photoCrawler.downloadPhotoExamples();
 //        photoCrawler.downloadPhotosForQuery(TOPICS.get(0));
-//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
     }
 }
--- Lab3-reactive-java/src/main/java/app/PhotoCrawler.java
+++ Lab3-reactive-java/src/main/java/app/PhotoCrawler.java
@@ -1,12 +1,14 @@
 package app;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import model.Photo;
 import util.PhotoDownloader;
 import util.PhotoProcessor;
 import util.PhotoSerializer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,10 +45,29 @@ public class PhotoCrawler {
     }
 
     public void downloadPhotosForQuery(String query) throws IOException {
-        // TODO Implement me :(
+        try {
+            photoDownloader.searchForPhotos(query)
+                    .take(10)
+                    .blockingSubscribe(photoSerializer::savePhoto);
+        } catch (InterruptedException e) {
+            log.log(Level.SEVERE, "Downloading photo examples error", e);
+        }
     }
 
     public void downloadPhotosForMultipleQueries(List<String> queries) {
-        // TODO Implement me :(
+        List<Observable<Photo>> list = new ArrayList<>();
+
+        try {
+            for (String query : queries) {
+                list.add(photoDownloader.searchForPhotos(query).subscribeOn(Schedulers.io()));
+            }
+        } catch (IOException | InterruptedException e) {
+            log.log(Level.SEVERE, "Downloading multiple photos error", e);
+        }
+
+        Observable.merge(list)
+                .take(15)
+                .blockingSubscribe(photoSerializer::savePhoto,
+                        e -> log.log(Level.SEVERE, "Error downloading photos", e));
     }
 }
