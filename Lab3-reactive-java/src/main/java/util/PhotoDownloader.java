f6ccbb0a51ce4f0ad26d97cded9e424cf6b7b651
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-10-19 19:58:30 +0200

    getPhotoExamples with optimized getPhoto usage

--- Lab3-reactive-java/src/main/java/util/PhotoDownloader.java
+++ Lab3-reactive-java/src/main/java/util/PhotoDownloader.java
@@ -23,11 +23,11 @@ public class PhotoDownloader {
 
     public Observable<Photo> getPhotoExamples() throws IOException {
         return Observable.just(
-                getPhoto("https://i.ytimg.com/vi/7uxQjydfBOU/hqdefault.jpg"),
-                getPhoto("http://digitalspyuk.cdnds.net/16/51/1280x640/landscape-1482419524-12382542-low-res-sherlock.jpg"),
-                getPhoto("http://image.pbs.org/video-assets/pbs/masterpiece/132733/images/mezzanine_172.jpg"),
-                getPhoto("https://classicmystery.files.wordpress.com/2016/04/miss-marple-2.jpg"),
-                getPhoto("https://i.pinimg.com/736x/7c/14/c9/7c14c97839940a09f987fbadbd47eb89--detective-monk-adrian-monk.jpg"));
+                "https://i.ytimg.com/vi/7uxQjydfBOU/hqdefault.jpg",
+                "http://digitalspyuk.cdnds.net/16/51/1280x640/landscape-1482419524-12382542-low-res-sherlock.jpg",
+                "http://image.pbs.org/video-assets/pbs/masterpiece/132733/images/mezzanine_172.jpg",
+                "https://classicmystery.files.wordpress.com/2016/04/miss-marple-2.jpg",
+                "https://i.pinimg.com/736x/7c/14/c9/7c14c97839940a09f987fbadbd47eb89--detective-monk-adrian-monk.jpg").map(this::getPhoto);
     }
 
     public List<Photo> searchForPhotos(String searchQuery) throws IOException, InterruptedException {
